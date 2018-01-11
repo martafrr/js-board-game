bd02a566dff185b407d551b84ffcc8657a11fb5e
Marta Farre Ribas <marta.farre@zalando.de>
2018-01-11 16:37:35 +0100

    database for level model

--- index.js
+++ index.js
@@ -33,38 +33,3 @@ app.listen(3000, () => {
 
 const addCartToLevel = (cardToAssign, levelTo) => (levels[levelTo].cards = levels[levelTo].cards.concat(cardToAssign));
 
-
-// instantiating some users
-//users = users.concat(new User('Marta', 'hi@marta.com', 26));
-//users = users.concat(new User('Andreas', 'hi@andreas.com', 33));
-
-// instantiating some cards
-// cards = cards.concat(new Card(
-//     'What is JavaScript?',
-//     'JavaScript is a cross-platform, object-oriented scripting language.',
-//     0
-// ));
-// cards = cards.concat(new Card(
-//     'What is the difference between undefined and not defined?',
-//     'Not defined means that it doesn\'t exist and has not been declared. However if you declare a variable and not define the value it holds, this var is undefined.',
-//     0
-// ));
-
-
-
-// const readFile = async (filename) => {
-//     return new Promise((resolve, reject) => {
-//         fs.readFile(filename, 'utf8', (err, content) => {
-//             if(err) return reject(err);
-//             resolve(content);
-//         })
-//     })
-// }
-
-// const file = [1].map(number => 'file.txt');
-
-// const main = async () => {
-//     const content1 = await readFile(file[0])
-//     console.log(content1)
-// }
-// main()
